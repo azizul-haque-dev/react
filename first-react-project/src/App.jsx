3936048e66aacd4e1d8c422d66f2f4e8324a7ff4
user-azizul <124841821+user-azizul@users.noreply.github.com>
2024-05-18 09:16:03 +0000

    testing react project

---
+++ first-react-project/src/App.jsx
@@ -0,0 +1,13 @@
+
+import Massage from "./Dynamic.jsx"
+import Random from './Random.jsx'
+function App() {
+
+
+  return <div> 
+     <h1>Vite + React</h1>;
+  <Massage></Massage>
+  <Random></Random>
+  </div> 
+}
+export default App;
