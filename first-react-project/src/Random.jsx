71cbacb2fb40a9c510b6bf9cde4e9966a25b7f91
user-azizul <azizul.contact.me@gmail.com>
2024-05-19 00:02:33 +0300

    fix the  bug

--- first-react-project/src/Random.jsx
+++ first-react-project/src/Random.jsx
@@ -1,6 +1,8 @@
- function Random (){
- let randomNum = Math.floor(Math.random*10)
- return <h2  style={{backgroundColor:'#f9f9f9'}}> Random num 
- is {randomNum}</h2>
- }
- export default Random;
+function Random() {
+  let randomNum = Math.floor(Math.random() * 10) + 3;
+
+  return (
+    <h2 style={{ backgroundColor: "#f9f9f9" }}> Random num is {randomNum}</h2>
+  );
+}
+export default Random;
