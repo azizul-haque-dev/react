44859350209e8fa61207c8f620b77a8475967c08
user-azizul <azizul.contact.me@gmail.com>
2024-05-20 00:55:13 +0300

    done todo ui

---
+++ react-project/1_todoUi_app/src/assets/UiComponent.jsx
@@ -0,0 +1,42 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+
+function Ui() {
+  let date = new Date();
+  let { firstTodo, firstDate, secTodo } = {
+    firstTodo: "Work Out",
+    firstDate: date.getDate(),
+    secTodo: "Sleep"
+  };
+  return (
+    <div className="container myContainer ">
+      <h2 className="title text-center mb-3"> Todo UI</h2>
+      <div className="row cus-row">
+        <div className="col-5">
+          <input type="text" placeholder="Enter todo item" />
+        </div>
+        <div className="col-5">
+          <input type="date" placeholder="Select date" />
+        </div>
+        <div className="col-2">
+          {" "}
+          <button className=" btn btn-success custom "> Add</button>
+        </div>
+      </div>
+      <div className="row cus-row">
+        <div className="col-5"> {firstTodo}</div>
+        <div className="col-5">{firstDate}</div>
+        <div className="col-2">
+          <button className=" btn btn-danger custom">Delete</button>
+        </div>
+      </div>{" "}
+      <div className="row cus-row">
+        <div className="col-5"> {secTodo}</div>
+        <div className="col-5">{firstDate}</div>
+        <div className="col-2">
+          <button className=" btn btn-danger custom">Delete</button>
+        </div>
+      </div>
+    </div>
+  );
+}
+export default Ui;
