50086dab84f293bb657df223f40509c7dc46ba11
user-azizul <azizul.contact.me@gmail.com>
2024-06-03 01:54:51 +0300

    fixes some npm issue and edit tailwind css config file for dark mode

--- react-project/09-themeChanger/src/App.jsx
+++ react-project/09-themeChanger/src/App.jsx
@@ -1,34 +1,33 @@
-import { useEffect, useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import { ThemeProvier } from "./Context/Theme";
+import Card from "./Component/Card";
+import ThemeBtn from "./Component/ThemeBtn";
+import "./App.css";
 
 function App() {
-  const [themeMode, setTheme] = useState('light')
-const darkMode = ()=> setTheme('dark')
-const lightMode= ()=> setTheme('light')
-useEffect(()=>{
-  const html = document.querySelector('html')
-  html.classList.remove('light','dark')
-  html.classList.add(themeMode)
-},[themeMode])
+  const [themeMode, setTheme] = useState("light");
+  const darkMode = () => setTheme("dark");
+  const lightMode = () => setTheme("light");
+  useEffect(() => {
+    const html = document.querySelector("html");
+    html.classList.remove("light", "dark");
+    html.classList.add(themeMode);
+  }, [themeMode]);
   return (
-    <ThemeProvider value = {{themeMode, darkMode,lightMode}}> 
-<div className="flex flex-wrap min-h-screen items-center">
-                <div className="w-full">
-                    <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
-                        
-                    </div>
+    <ThemeProvier value={{ themeMode, darkMode, lightMode }}>
+      <div className="flex flex-wrap min-h-screen items-center">
+        <div className="w-full">
+          <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
+            <ThemeBtn></ThemeBtn>
+          </div>
 
-                    <div className="w-full max-w-sm mx-auto">
-                       
-                    </div>
-                </div>
-            </div>
-
-            </ThemeProvider>
-   
-  )
+          <div className="w-full max-w-sm mx-auto">
+            <Card></Card>
+          </div>
+        </div>
+      </div>
+    </ThemeProvier>
+  );
 }
 
-export default App
+export default App;
--- react-project/09-themeChanger/src/Context/Theme.js
+++ react-project/09-themeChanger/src/Context/Theme.js
@@ -2,7 +2,7 @@ import { createContext,useContext } from "react";
 export const ThemeContext = createContext({
     themeMode:'light',
     darkMode:()=>{},
-    lightMode:()=>{}
+    lightMode:()=>{} 
 })
 export const ThemeProvier = ThemeContext.Provider;
 export function useTheme(){
