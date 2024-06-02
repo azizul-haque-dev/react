6a4d600af3c7fcd0e7315ae6b6ec9c0290e47edd
user-azizul <124841821+user-azizul@users.noreply.github.com>
2024-06-02 22:15:44 +0000

    theme changer project done useing context api

--- react-project/09-themeChanger/src/App.jsx
+++ react-project/09-themeChanger/src/App.jsx
@@ -1,13 +1,19 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const [themeMode, setTheme] = useState('light')
+const darkMode = ()=> setTheme('dark')
+const lightMode= ()=> setTheme('light')
+useEffect(()=>{
+  const html = document.querySelector('html')
+  html.classList.remove('light','dark')
+  html.classList.add(themeMode)
+},[themeMode])
   return (
-   
+    <ThemeProvider value = {{themeMode, darkMode,lightMode}}> 
 <div className="flex flex-wrap min-h-screen items-center">
                 <div className="w-full">
                     <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
@@ -20,7 +26,8 @@ function App() {
                 </div>
             </div>
 
-
+            </ThemeProvider>
+   
   )
 }
 
