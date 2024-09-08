cfc81b5f70358f586aa252c939f3225870271e1f
user-azizul <124841821+user-azizul@users.noreply.github.com>
2024-09-08 10:55:21 +0000

    about and contact page done

--- react-ecommerce/src/components/Footer.jsx
+++ react-ecommerce/src/components/Footer.jsx
@@ -4,7 +4,7 @@ import { assets } from "../assets/frontend_assets/assets";
 function Footer() {
   return (
     <div>
-      <div className="flex flex-col sm:grid grid-cols-[3fr_1fr_1fr] gap-14 my-10 mt-40 text-sm">
+      <div className="flex flex-col sm:grid grid-cols-[3fr_1fr_1fr] gap-14 my-10 mt-36 text-sm">
         <div>
           <img src={assets.logo} className="w-32 mb-5" alt="" />
           <p className="w-full md:2/3 text-gray-600">
--- react-ecommerce/src/pages/Contact.jsx
+++ react-ecommerce/src/pages/Contact.jsx
@@ -1,7 +1,42 @@
 import React from "react";
+import { assets } from "../assets/frontend_assets/assets";
+import Title from "../components/Title";
+import NewsLaterBox from "../components/NewsLaterBox";
 
 const Contact = () => {
-  return <div>contact</div>;
+  return(
+    <div>
+    <div className="border-t pt-8 text-2xl text-center">
+      <Title text1={"CONTACT"} text2={"US"} />
+    </div>
+    <div className="flex my-10 gap-16 flex-col md:flex-row mb-20">
+      <img
+        src={assets.about_img}
+        alt="about-img"
+        className="w-full md:max-w-[450px] "
+      />
+      <div className="flex flex-col items-start justify-center gap-6 md:w-2/4 text-gray-600">
+        <b className="text-xl font-semibold text-gray-600">Our Store</b>
+        <p>
+        54709 Willms Station <br />
+Suite 350, Washington, USA
+        </p>
+        <p>
+        Tel: (415) 555-0132 <br />
+        Email: admin@forever.com
+        </p>
+        <b className="text-xl font-semibold text-gray-600">Careers at Forever</b>
+        <p>
+        Learn more about our teams and job openings.
+        </p>
+        <button className="border border-black px-8 py-4 text-sm hover:bg-black hover:text-white transition-all duration-500"> Explore Jobs</button>
+      </div>
+    </div>
+  
+   
+    <NewsLaterBox/>
+  </div>
+  )
 };
 
 export default Contact;
